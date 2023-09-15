1cd6ca820643f4ac5aaf4b36944c5b84aebde545
chuan <1516998446@qq.com>
2023-09-15 23:53:07 +0800

    9.15 23:52，复习

--- guess.c
+++ guess.c
@@ -3,26 +3,28 @@
 // by HHC
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 int main(void)
 {                   // print the rule of the game
     int high = 100; // 最大数
     int chance = 7;
-    printf("the computer will generate a random number between 1 and %d \n"
-           "you have %d chances",
+    printf("the computer will generate a random number between 1 and %d ,\n"
+           "you have %d chances\n\n",
            high, chance);
-
+    // printf("chance_before=%d", chance);
     // generate a random number
-
     srand(time(NULL)); // use current time as seed for random generator
+    // int random_variable = rand();
     int secret = rand() % high + 1;
 
     // let the player enter his/her guess number
     int guess;
-    printf("enter your guess number\n");
-    scanf("%d", &guess);
-    while (chance > 0)
+
+    while (chance >= 0)
     {
+        printf("enter your guess number\n");
+        scanf("%d", &guess);
         chance--;
         if (guess == secret)
         {
@@ -34,9 +36,16 @@ int main(void)
             printf("guess>secret\n");
         }
         else
-            printf("guess<secret");
-        
-    }
+        {
+            printf("guess<secret\n");
+        };
+        if (chance == 0)
+        {
+            printf("you lose the game\n ");
+            break;
+        };
+        // printf("change=%d",chance);
+    };
 
     // store the gurss number
     // compare it with the secret number
@@ -44,6 +53,5 @@ int main(void)
 
     // loop:repeat until the player wins or loses
 
-    printf("rule of the game\n");
     return 0;
 }
